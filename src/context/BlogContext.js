14588dc1a059d13ca9bcabb766fa71e9d68a4fd6
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-24 22:19:50 +0400

    App,js, Context Object and Index Screen Created

---
+++ src/context/BlogContext.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BlogContext = React.createContext();
+
+const BlogProvider = ({ children }) => {
+    return <BlogContext.Provider>
+        {children}
+    </BlogContext.Provider>
+};
