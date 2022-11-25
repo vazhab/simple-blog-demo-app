3355f5bc766d9e49a48d6cfee20b1cb423ad238a
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-25 18:33:35 +0400

    Now its possible to add posts by clicking + button

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,32 +1,36 @@
-import React, { useReducer } from 'react';
 import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'delete_blogpost':
-      return state.filter((blogPost) => blogPost.id !== action.payload);
+      return state.filter(blogPost => blogPost.id !== action.payload);
 
     case 'add_blogpost':
-      return [...state, {
-        id: Math.floor(Math.random() * 99999),
-        title: `Blog Post ${state.length + 1}`
-      }]
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content
+        }
+      ];
     default:
       return state;
   }
 };
 
-const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: 'add_blogpost' });
-  }
+const addBlogPost = dispatch => {
+  return (title, content, callback) => {
+    dispatch({ type: 'add_blogpost', payload: { title, content } });
+    callback();
+  };
 };
 
 const deleteBlogPost = dispatch => {
-  return (id) => {
-    dispatch({ type: 'delete_blogpost', payload: id })
+  return id => {
+    dispatch({ type: 'delete_blogpost', payload: id });
   };
-}
+};
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
