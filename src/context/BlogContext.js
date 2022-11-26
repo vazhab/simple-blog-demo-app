329ebe39a8741de15d12ebcc8dc54e4db2978a4e
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-26 20:45:23 +0400

    delete blogpost functionality added

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -14,16 +14,6 @@ const blogReducer = (state, action) => {
 
     case 'delete_blogpost':
       return state.filter(blogPost => blogPost.id !== action.payload);
-
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
     default:
       return state;
   }
@@ -40,13 +30,14 @@ const getBlogPosts = dispatch => {
 const addBlogPost = dispatch => {
   return async (title, content, callback) => {
     await jsonServer.post('/blogposts', { title, content });
-    // dispatch({ type: 'add_blogpost', payload: { title, content } });
     if (callback) { callback(); }
   };
 };
 
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
+    
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
